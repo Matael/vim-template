b2b1be8d8bf3b75505a4f8be8978f2481b9e7cd7
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-03-08 10:46:43 +0100

    Adapatation des templates

--- templates/template.php
+++ templates/template.php
@@ -1,15 +1,32 @@
 <?php
-/**
- * Short description for %FFILE%
+/*
+ * %FFILE%
  *
- * Copyright (C) %YEAR% %USER% <%MAIL%>
+ * Copyright (C) %YEAR% Mathieu Gaborit (matael) <mathieu@matael.org>
  *
- * Distributed under terms of the MIT license.
+ *
+ * Distributed under WTFPL terms
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                    Version 2, December 2004
+ *
+ * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ *
+ * --
  *
  * @package %FILE%
- * @author %USER% <%MAIL%>
+ * @author Mathieu Gaborit (matael) <mathieu@matael.org>
  * @version 0.1
- * @copyright (C) %YEAR% %USER% <%MAIL%>
+ * @copyright (C) %YEAR% Mathieu Gaborit (matael) <mathieu@matael.org>
  */
 
 %HERE%
--- templates/template.py
+++ templates/template.py
@@ -1,11 +1,30 @@
 #! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
+# -*- coding:utf8 -*-
 #
-# Copyright © %YEAR% %USER% <%MAIL%>
+# %FFILE%
 #
-# Distributed under terms of the MIT license.
+# Copyright © %YEAR% Mathieu Gaborit (matael) <mathieu@matael.org>
+#
+#
+# Distributed under WTFPL terms
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+#
 
 """
 %HERE%
 """
+
+
